303e13c3561cdd260bac64eaac6c6b4621f4c51e
刘志伟 <lzw@spacetech.com.cn>
2021-10-19 14:17:21 +0800

    优化

--- 27. 移除元素/index.ts
+++ 27. 移除元素/index.ts
@@ -1,22 +1,20 @@
-// 题目: https://leetcode-cn.com/problems/remove-element/
 /**
+ * 题目: https://leetcode-cn.com/problems/remove-element/
+ * 注意 外部方法会根据返回的length对数组做截取，所以无需手动截取
  * 思路: 由于题目要求在原数组操作，所以可以将查询到的内容统一放到队首或者队尾
  *      然后在依此出队,返回数组长度
  *
  */
 function removeElement(nums: number[], val: number): number {
   let headIndex = 0
-  let tempValue = null
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === val) {
-      tempValue = nums[i]
-      nums[i] = nums[headIndex]
-      nums[headIndex] = tempValue
+  let tailIndex = nums.length
+  while (tailIndex !== headIndex) {
+    if (nums[headIndex] === val) {
+      nums[headIndex] = nums[tailIndex - 1]
+      tailIndex--
+    } else {
       headIndex++
     }
   }
-  for (let i = 0; i < headIndex; i++) {
-    nums.shift()
-  }
-  return nums.length
+  return tailIndex
 }
